62322e67d3f8ddd066b79f47eda15f08be6e23ea
Ahmed Khedr <94747792+Developer-Ahmed-Ayman-khedr@users.noreply.github.com>
2024-05-06 18:47:19 +0300

    Initialize Code file.

---
+++ Code/Code/main.c
@@ -0,0 +1,18 @@
+/*
+ * Code.c
+ *
+ * Created: 5/6/2024 6:46:37 PM
+ * Author : ahmed
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
