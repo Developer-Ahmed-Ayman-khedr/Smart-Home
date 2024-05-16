5d1c36631aa7eac477da0ab8012105819311f6ed
Ahmed Khedr <94747792+Developer-Ahmed-Ayman-khedr@users.noreply.github.com>
2024-05-16 19:36:40 +0300

    Initialize DIO Driver.

---
+++ MCAL/DIO/DIO_INT.h
@@ -0,0 +1,13 @@
+/*
+ * DIO_INT.h
+ *
+ *  Created on: May 16, 2024
+ *      Author: ahmed
+ */
+
+#ifndef MCAL_DIO_DIO_INT_H_
+#define MCAL_DIO_DIO_INT_H_
+
+
+
+#endif /* MCAL_DIO_DIO_INT_H_ */
---
+++ MCAL/DIO/DIO_PORG.c
@@ -0,0 +1,8 @@
+/*
+ * DIO_PORG.c
+ *
+ *  Created on: May 16, 2024
+ *      Author: ahmed
+ */
+
+
