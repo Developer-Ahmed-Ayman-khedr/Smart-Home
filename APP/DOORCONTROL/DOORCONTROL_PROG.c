43e51f165212f02b271e06e571e98b2d1663cf9e
Ahmed Khedr <94747792+Developer-Ahmed-Ayman-khedr@users.noreply.github.com>
2024-05-26 20:12:34 +0300

    Update.

--- APP/DOORCONTROL/DOORCONTROL_PROG.c
+++ APP/DOORCONTROL/DOORCONTROL_PROG.c
@@ -12,24 +12,33 @@ void DOORCONTROL_init(){
 	//Servo Motor control PIN
 	DIO_setPinDir(DIO_PIND5,DIO_OUTPUT);
 
+	//Timer1
+	GI_enable();
+
 	TIMER1_initFastPwmMod14();
 
-	//TIMER1_setFrequency(50);
+	TIMER1_setFrequency(50);
 
 }
 
 void DOORCONTROL_Start(u8* Ma_Fl_ptr){
+
 	//Open the door
 	TIMER1_setOcr(499);
 
+	_delay_ms(1000);
+	//Close the door
+	TIMER1_setOcr(375);
+
+	_delay_ms(1000);
 	//Wait for the Ultrasonic Sensor to return a distance greater than 30cm
-	HOLD_Start();
-	LCD_sendNum(HOLD_Retrun());
+	//HOLD_Start();
+	//LCD_sendNum(HOLD_Retrun());
 
-	if(HOLD_Retrun()>30){
-		//Close the door
-		TIMER1_setOcr(375);
+	//if(HOLD_Retrun()>30)
+	{
 
-		* Ma_Fl_ptr = 5 ;
+
+		//* Ma_Fl_ptr = 5 ;
 	}
 }
