54e5596234d01a89005ea756608561351eb2cabc
abdoelrazaz <165102960+abdoelrazaz@users.noreply.github.com>
2024-07-28 21:48:47 +0300

    Update.

--- APP/USERCONTROL/USERCONTROL_PROG.c
+++ APP/USERCONTROL/USERCONTROL_PROG.c
@@ -60,71 +60,20 @@ void AddUser()
 }
 
 BOOL CheckDataForUser(){
-<<<<<<< HEAD
-<<<<<<< HEAD
-	u8 KPD_RecevedData , User_Data[5] , Userindex = 0 , EEPROMRecievedData , AccessTimes=1;
-	u16 EEPROMIndex =MINEEPROMUSER;
-	BOOL AccessResult = FALSE ;
-	while (Userindex<=4){
-=======
-	BOOL flage=FALSE,EEPROM_flage = FALSE,Save_flage = FALSE , flag=0 , Check_index = MINEEPROMUSER + 4;
-	u8  KPD_RecevedData, Entered_User[1],Entered_Pass[4], Userindex = 0,Passindex=0, i2 = 0, EEPROMRecevedData,counter=0;
-
-	while (i2<4)
-	{
-		_delay_ms(100);
-		EEPROM_ReadByteNACK(&EEPROMRecevedData,i2);
-		if (EEPROMRecevedData!=255)
-		{
-			EEPROM_flage = TRUE;
-		}
-		else
-		{
-			EEPROM_flage = FALSE;
-		}
-		i2++;
-	}
-
->>>>>>> master
-=======
 	u8 KPD_RecevedData , User_Data[5] , Userindex = 0 , EEPROMRecievedData , AccessTimes=1; 
 	u16 EEPROMIndex =MINEEPROMUSER;
 	BOOL AccessResult = FALSE ; 
 	while (Userindex<=4){
->>>>>>> 89dd561784cd40646aa88ca0df04c0ab8c45c750
 	KPD_RecevedData = KPD_read();
 	if ( KPD_RecevedData!=KPD_UNPRESSED)
 	{
 		while(KPD_read()!=KPD_UNPRESSED);
-<<<<<<< HEAD
-<<<<<<< HEAD
-		LCD_sendNum(KPD_RecevedData-48);
-		User_Data[Userindex] = KPD_RecevedData;
-		Userindex++;
-=======
-
-			LCD_sendNum(KPD_RecevedData-48);
-			Entered_User[Userindex] = KPD_RecevedData;
-
-			Userindex++;
->>>>>>> master
-	}
-	}
-<<<<<<< HEAD
-	while (EEPROMIndex<MAXEEPROMUSER)
-=======
-
-
-	if (Userindex==1&&flag==0)
->>>>>>> master
-=======
 		LCD_sendNum(KPD_RecevedData-48);
 		User_Data[Userindex] = KPD_RecevedData;
 		Userindex++;
 	}
 	}
 	while (EEPROMIndex<MAXEEPROMUSER)
->>>>>>> 89dd561784cd40646aa88ca0df04c0ab8c45c750
 	{
 		_delay_ms(100); 
 		EEPROM_ReadByteNACK(&EEPROMRecievedData,EEPROMIndex);
@@ -132,7 +81,7 @@ BOOL CheckDataForUser(){
 		{
 			for (Userindex=0 ; Userindex<5;Userindex++)
 			{
-				_delay_ms(50)
+				_delay_ms(50);
 				EEPROM_ReadByteNACK(&EEPROMRecievedData , EEPROMIndex) ; 
 				if (EEPROMRecievedData == User_Data[Userindex])
 				{
@@ -148,6 +97,7 @@ BOOL CheckDataForUser(){
 		}
 		EEPROMIndex+=5;
 	}
+
 	if (AccessResult == TRUE)
 	{
 		LCD_sendStr("welcome/r/n") ;		
